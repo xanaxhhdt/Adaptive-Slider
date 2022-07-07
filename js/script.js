a53cd02c6f502eb0956c001906c3c6af807d44a8
Vladislav <nicochan00@mail.ru>
2022-07-07 18:24:19 +0300

    slider realize button next

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const images = document.querySelectorAll('.slider .slider-line img');
+const sliderLine = document.querySelector('.slider-line');
+
+let count = 0;
+let width;
+
+function init() {
+   width = document.querySelector('.slider').offsetWidth;
+   sliderLine.style.width = width * images.length + 'px';
+
+   images.forEach(item => {
+      item.style.width = width + 'px';
+      item.style.height = 'auto';
+   });
+}
+
+window.addEventListener('resize', init);
+init();
+
+document.querySelector('.slider-next').addEventListener('click', () => {
+   count++;
+   if (count >= images.length) {
+      count = 0;
+   }
+   rollSlider();
+});
+
+function rollSlider() {
+   sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
